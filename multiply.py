10fef6d21a6fad40938e4cc3f78828c87fca5325
Francis Murungi <francis_951@live.com>
2023-06-25 20:45:36 +0300

    I have written a passing test

--- multiply.py
+++ multiply.py
@@ -27,4 +27,16 @@
 #             int: The product of num1 and num2.
 #         """
 #         return self.num1 * self.num2
-    
+
+"""
+this method finds product of the two numbers
+"""
+def multiply_one(num_1, num_2):
+    """
+    it takes two param
+    :param: num_1 (int) => first number
+    :param: num_2 (ijnt) => second number
+    then it renturns the product of the two
+    return : int
+    """
+    return num_1 * num_2
