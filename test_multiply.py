f57e33fbc51c5e65b6d62281a4780d0596e7283e
Francis Murungi <francis_951@live.com>
2023-06-25 21:08:11 +0300

    added a passing test

--- test_multiply.py
+++ test_multiply.py
@@ -22,6 +22,16 @@ from multiply import multiply_one
 """
 the module to do the test
 """
+
 def test_multiply_one():
+    """
+    the check for the assert
+    """
+
     assert multiply_one(1, 1) == 1
     assert multiply_one(2, 2) == 4
+    assert multiply_one(3, 3) == 9
+    assert multiply_one(4, 4) == 16
+    assert multiply_one(23, 45) == 1035
+
+    
